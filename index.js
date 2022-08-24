9f3edde270c569b66ded3f7143dad5d91a1ed77c
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-08-24 10:46:43 -0600

    aaaaah random logic works

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ negative_keywords = ["sad", "depressed", "unhappy", "angry", "miserable", "fuck"
 
 const respones = ["My reply is no","Don't count on it","My sources say no","Very doubtful","Outlook not so good","Better not tell you now","Cannot predict now","Ask again later","Reply hazy try again","Concentrate and ask again","Outlook good","Most likely","Signs point to yes","As I see it, yes","Yes","You may rely on it","Without a doubt","Yes, definitely","It is decidedly so","It is certain"]
 
-function getResponse(query)
-    if (negative_keywords.some(word => query.includes(word))) {
-        const negativeReply = negative_replies[Math.floor(Math.random() * negative_replies.length)]
+const query = document.getElementById("text-input")
+const submit = document.getElementById("form-submit")
+
+function getResponse(evt){
+    evt.preventDefault()
+    console.log(query)
+    let queryText = query.value
+    if (negative_keywords.some(word => queryText.includes(word))) {
+        const negativeReply = respones[Math.floor(Math.random() * respones.length)]
         console.log(negativeReply)
     }
+}
+
+submit.addEventListener("click", getResponse)
