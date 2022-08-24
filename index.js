a8671a892423c269ce6deeffca8440db874f5cab
Riley Shipman <ishipman@student.neumont.edu>
2022-08-24 10:36:28 -0600

    stuff

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 negative_keywords = ["sad", "depressed", "unhappy", "angry", "miserable", "fuck", "shit", "pissed", "not vibing"];
 
-negative_replies = [
-    "Vibes are attrocious.",
-    "Yo ur vibes are all off my guy",
-    "fix ur vibes at once pls",
-    "gwib",
-    "no",
-    "no <3",
-    "yea alright",
-    "swiggity swooty fix your fuckin vibes at once"
-]
+
+const respones = ["My reply is no","Don't count on it","My sources say no","Very doubtful","Outlook not so good","Better not tell you now","Cannot predict now","Ask again later","Reply hazy try again","Concentrate and ask again","Outlook good","Most likely","Signs point to yes","As I see it, yes","Yes","You may rely on it","Without a doubt","Yes, definitely","It is decidedly so","It is certain"]
 
 function getResponse(query)
     if (negative_keywords.some(word => query.includes(word))) {
