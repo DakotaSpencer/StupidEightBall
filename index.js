3d67b811a4917e6873bdd7cdd1bfee07329e00c2
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-08-25 15:29:40 -0600

    hehe keywords

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-negative_keywords = ["sad", "depressed", "unhappy", "angry", "miserable", "fuck", "shit", "pissed", "not vibing"];
-
+no_keywords = ["should","new","does","famous","millionaire","promotion","marry","pass","crush","love","rich","will","give"];
+yes_keywords = ["gay"]
 
 const respones = ["My reply is no","Don't count on it","My sources say no","Very doubtful","Outlook not so good","Better not tell you now","Cannot predict now","Ask again later","Reply hazy try again","Concentrate and ask again","Outlook good","Most likely","Signs point to yes","As I see it, yes","Yes","You may rely on it","Without a doubt","Yes, definitely","It is decidedly so","It is certain"]
 
@@ -10,7 +10,7 @@ function getResponse(evt){
     evt.preventDefault()
     console.log(query)
     let queryText = query.value
-    if (negative_keywords.some(word => queryText.includes(word))) {
+    if (no_keywords.some(word => queryText.includes(word))) {
         const negativeReply = respones[Math.floor(Math.random() * respones.length)]
         console.log(negativeReply)
     }
