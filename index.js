7321f5527cd5b1a80d6a7f379fd2f8f81c198d11
Riley Shipman <ishipman@student.neumont.edu>
2022-08-29 10:15:24 -0600

    stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 negative_keywords = ["sad", "depressed", "unhappy", "angry", "miserable", "fuck", "shit", "pissed", "not vibing"];
+positive_keywords = ["sad", "depressed", "unhappy", "angry", "miserable", "fuck", "shit", "pissed", "not vibing"];
 
 
 const respones = ["My reply is no","Don't count on it","My sources say no","Very doubtful","Outlook not so good","Better not tell you now","Cannot predict now","Ask again later","Reply hazy try again","Concentrate and ask again","Outlook good","Most likely","Signs point to yes","As I see it, yes","Yes","You may rely on it","Without a doubt","Yes, definitely","It is decidedly so","It is certain"]
@@ -13,6 +14,16 @@ function getResponse(evt){
     if (negative_keywords.some(word => queryText.includes(word))) {
         const negativeReply = respones[Math.floor(Math.random() * respones.length)]
         console.log(negativeReply)
+    } else if (positive_keywords.some(word => queryText.includes(word))) {
+
+    } else {
+
+    }
+}
+
+const getWeightedResponse = (positive) => {
+    if (positive) {
+        return Math.random()*(.75)
     }
 }
 
