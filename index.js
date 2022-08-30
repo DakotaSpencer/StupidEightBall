204dcbf44f950b93b8b23f1f440856ce18824d4a
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-08-30 10:49:42 -0600

    Added 3d-ish styling to magic eight ball

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ keywords = ["I", "me", "my", "where", "why","do","should","new","does","famous",
 positive_keywords = ["gay"]
 negative_keywords = ["sad", "depressed", "unhappy", "angry", "miserable", "fuck", "shit", "pissed", "not vibing"];
 
-const respones = ["My reply is no","Don't count on it","My sources say no","Very doubtful","Outlook not so good","Better not tell you now","Cannot predict now","Ask again later","Reply hazy try again","Concentrate and ask again","Outlook good","Most likely","Signs point to yes","As I see it, yes","Yes","You may rely on it","Without a doubt","Yes, definitely","It is decidedly so","It is certain"]
+const responses = ["My reply is no","Don't count on it","My sources say no","Very doubtful","Outlook not so good","Better not tell you now","Cannot predict now","Ask again later","Reply hazy try again","Concentrate and ask again","Outlook good","Most likely","Signs point to yes","As I see it, yes","Yes","You may rely on it","Without a doubt","Yes, definitely","It is decidedly so","It is certain"]
 
 const query = document.getElementById("text-input")
 const submit = document.getElementById("form-submit")
 
 const responseText = document.getElementById("response-text")
-
-const response = document.getElementById('response')
 
 
 function getResponse(evt){
@@ -20,25 +18,24 @@ function getResponse(evt){
     let queryText = query.value.toLowerCase()
     let reply
     if (negative_keywords.some(word => queryText.includes(word))) {
-        reply = respones[getWeightedResponse(false)]
+        reply = responses[getWeightedResponse(false)]
         console.log(reply)
     } else if (positive_keywords.some(word => queryText.includes(word))) {
-        reply = respones[getWeightedResponse(true)]
+        reply = responses[getWeightedResponse(true)]
         console.log(reply)
     } else {
-        reply = respones[Math.floor(Math.random()*respones.length)]
+        reply = responses[Math.floor(Math.random()*responses.length)]
         console.log(reply)
     }
-    response.innerHTML = reply
     responseText.innerText = reply
     query.value = ""
 }
 
 const getWeightedResponse = (positive) => {
     if (positive) {
-        return Math.floor(Math.random()*(.75*respones.length))+(Math.floor(.25*respones.length))
+        return Math.floor(Math.random()*(.75*responses.length))+(Math.floor(.25*responses.length))
     } else {
-        return Math.floor(Math.random()*(.75*respones.length))
+        return Math.floor(Math.random()*(.75*responses.length))
     }
 }
 
